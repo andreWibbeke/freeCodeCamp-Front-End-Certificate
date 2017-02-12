890520af742ea283c9f8cf76955baa0cdbf2a82a
André <andre.wibbeke@gmail.com>
2017-02-12 10:48:53 +0100

    Create arguments_optional.js
    https://www.freecodecamp.com/challenges/arguments-optional

---
+++ arguments_optional.js
@@ -0,0 +1,21 @@
+function addTogether() {
+  //Variable and subroutine declaration (optional, but makes code cleaner)
+  var args = arguments;
+  var a = args[0];
+  var b = args[1];
+  function isNum(num) {
+    return Number.isInteger(num);
+  }
+
+  //Actual program: relies on implicit return of 'undefined'
+  //Note: while refactoring I remove curly braces if not required
+  if (isNum(a)) {
+    if (isNum(b))
+      return a + b;
+    else if (!b)
+      return function(b) {
+        if (isNum(b))
+          return a + b;
+      };
+  }
+}
