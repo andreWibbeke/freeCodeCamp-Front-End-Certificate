729ed11d22577e23f875b36771f8ede9b787d00b
André <andre.wibbeke@gmail.com>
2017-02-14 18:55:24 +0100

    Create Record collection.js

---
+++ advanced-algorithm-scripting/Record collection.js
@@ -0,0 +1,18 @@
+//https://www.freecodecamp.com/challenges/record-collection
+
+function updateRecords(id, prop, value) {
+  //making use of the fact, that every value is treated as "truthy" as long as it isn't "falsey"
+  //instead of having large conditions using logical operators, I'm using a more readable, simple if/else logic
+  if (value) {
+    if (prop == "tracks") {
+      if (collection[id][prop])
+        collection[id][prop].push(value);
+      else
+        collection[id][prop] = [value];
+    } else
+      collection[id][prop] = value;
+  } else
+    delete collection[id][prop];
+
+  return collection;
+}
