5118dc0e9068dde38a73049963530286a9494c31
André <andre.wibbeke@gmail.com>
2017-03-14 11:48:10 +0100

    Update Pairwise.js

--- advanced-algorithm-scripting/Pairwise.js
+++ advanced-algorithm-scripting/Pairwise.js
@@ -1,15 +1,15 @@
 //https://www.freecodecamp.com/challenges/pairwise
 
 function pairwise(arr, arg) {
-  var indicesSum = 0;
+  var indicesSum = 0, length = arr.length;
 
-  for (var i = 0; i < arr.length; i++) {
-    for (var j = i + 1; j < arr.length; j++) {
+  for (var i = 0; i < length; i++) {
+    for (var j = i + 1; j < length; j++) {
       if ((arr[i] + arr[j]) === arg) {
         indicesSum += (i + j);
         //invalidate the number at index 'j' used for pairing (only 'j', since 'i' won't be used again anyway)
-        arr.splice(j, 1, arg + 1);
-        //break the loop as soon as a pair is found
+        arr[j] = NaN;
+        //break the loop as soon as a pair is found (no need to keep running)
         break;
       }
     }
